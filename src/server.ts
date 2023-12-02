1b91ab7e16b81b99f6d7dd82a4b28438008d3e35
Enildo Andrade <enildoandrade@outlook.com>
2023-12-02 19:26:22 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { app } from './app'
+import { env } from './env'
+
+app
+  .listen({
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+    port: env.PORT,
+  })
+  .then(() => console.log('Http Server Running!'))
